61d2d6f3d1d98a5fdfb176523dc67da053a0a614
Alex <roboshadow@mail.ru>
2021-12-27 01:13:17 +0300

    add dz1 solution

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArray(int n)
+{
+    int[] arr = new int[n];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 20);
+    }
+    return arr;
+}
+
+void ShowArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write(arr[i] + " ");
+}
+
+
+int EvenNumbersCounter(int[] list)
+{
+    int count = 0;
+    for (int i = 0; i < list.Length; i++)
+    {
+        if (list[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] OnlyEvenNumbers(int[] arr, int num)
+{
+    int[] newArray = new int[num];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            newArray[j] = arr[i];
+            j++;
+        }
+    }
+    return newArray;
+
+}
+
+void ShowEvenNumbersArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int[] arr1 = CreateArray(10);
+ShowArray(arr1);
+Console.WriteLine();
+int count = EvenNumbersCounter(arr1);
+int[] arr2 = OnlyEvenNumbers(arr1, count);
+ShowEvenNumbersArray(arr2);
+
